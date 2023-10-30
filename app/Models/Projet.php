d68dcc5ecf05ab3e9666dd1a93910c36aaeee844
Antonio <eliseanto148@gmail.com>
2023-10-30 08:17:23 +0000

    deuxieme commit sur le front

--- app/Models/Projet.php
+++ app/Models/Projet.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Projet extends Model
 {
     use HasFactory;
+
+    public function taches() {
+        return $this->hasMany(TaskController::class);
+    }
+
+    public function calculerStatut() {
+        $taches = $this->tasks; 
+
+        if ($taches->count() === 0) {
+            return 'En attente';
+        }
+
+        $toutesTerminees = $taches->every(function ($tache) {
+            return $tache->statut === 'Terminée';
+        });
+
+        if ($toutesTerminees) {
+            return 'Terminé';
+        }
+
+        return 'En cours';
+    }
 }
